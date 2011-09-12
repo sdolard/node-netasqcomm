85d707cf98a71f5698396f722f7cdc0c434d1a56
Sébasten Dolard <sdolard@gmail.com>
2011-09-12 12:54:47 +0200

    example update (cf error event modification)

--- examples/basic.js
+++ examples/basic.js
@@ -4,12 +4,12 @@ session = new netasqComm.Session('admin', 'adminadmin', '10.0.0.254');
 
 //session.verbose = true; // true if you want debug logs
 
-session.on('error', function(error, errorString) {
-		if (isNaN(error)) {
-			console.log('session error: %s', error.message);
+session.on('error', function(error) {
+		if (error) {
+			console.log('Session error (%s): %s', error.code, error.message);	
 			return;
 		}
-		console.log('session error: %s (%d)', errorString, error);		
+		console.log('Session error occured (no details)');			
 });
 
 session.connect(function() {
