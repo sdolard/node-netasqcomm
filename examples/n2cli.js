8f542a818ad03e4a9add0345d67d148b1db3da2b
Sébasten Dolard <sdolard@gmail.com>
2011-09-28 23:02:25 +0200

    Adding comments, cleaning code...

--- examples/n2cli.js
+++ examples/n2cli.js
@@ -36,12 +36,27 @@ optParser, opt, promptArray = [],
 * Session
 */
 session = new netasqComm.Session();
+session.on('error', function(error) {
+		if (error) {
+			console.log('Session error (%s): %s', error.code, error.message);	
+			process.exit(1);
+			return;
+		}
+		console.log('Session error occured (no details)');
+		process.exit(1);
+});
 
+/**
+* Uncaught exception 
+*/
 process.on('uncaughtException', function (exception) {
 		console.error('Process uncaught exception: ', exception.message);
 });
 
 
+/**
+* Display help
+*/
 function displayHelp() {
 	console.log('n2cli.js [–v] [–h] [–s] [-d] [–a address] [-l login] [-p password] [-P port]');
 	console.log('NETASQ node cli example.');
@@ -55,7 +70,9 @@ function displayHelp() {
 	console.log('  d: disable ssl');
 }
 
-// Option
+/**
+* Command line options
+*/
 optParser = new getopt.BasicParser(':hdva:l:p:P:', process.argv);
 while ((opt = optParser.getopt()) !== undefined && !opt.error) {
 	switch(opt.option) {
@@ -94,20 +111,115 @@ while ((opt = optParser.getopt()) !== undefined && !opt.error) {
 	}
 }
 
+/**
+* Verbose mode
+*/
 if (session.verbose) {
 	console.log('Verbose enabled');
 }
 
-session.on('error', function(error) {
-		if (error) {
-			console.log('Session error (%s): %s', error.code, error.message);	
-			process.exit(1);
-			return;
+/**
+* Manage response
+*   - data to display
+*   - file upload/download
+*/
+function manageResponse(data) {
+	var serverd, fileSize, fileCRC, fileName, fileWs;
+	
+	switch (parseInt(netasqComm.getObjectValue('nws.code', data),10)) {
+	case netasqComm.NWS_ERROR_CODE.OK: 
+		serverd = netasqComm.getObjectValue('nws.serverd', data);
+		netasqComm.dumpServerdObject(serverd);
+		
+		switch(parseInt(serverd.ret, 10)) {
+			// Success: serverd is waiting for data
+		case netasqComm.SERVERD.OK_SERVER_WAITING_MULTI_LINES: 
+			prompt.get([
+					{
+						message: 'File to upload',    
+						name: 'file'/*, 
+						default: '/Users/sebastiend/Dev/perso/node/node-netasq-comm/download_config-backup-list=object_V50XXA0H0000003'  */         
+					}
+			], function (err, result) {
+				if (err) {
+					return;
+				}
+				console.log('Uploading %s...', result.file);
+				session.upload(result.file, manageResponse);
+			});
+			
+			break;
+			
+			// Disconnected
+		case netasqComm.SERVERD.OK_DISCONNECTED: // Success: Session is closed
+		case netasqComm.SERVERD.KO_AUTH: // Authentication failed
+		case netasqComm.SERVERD.KO_TIMEOUT_DISCONNECTED: // Failure: timout disonnected (no activity)
+		case netasqComm.SERVERD.KO_MAXIMUM_ADMIN_REACH: // Failure: maximum administrator are connected to appliance
+			break;
+			
+			// Multiple lines
+		case netasqComm.SERVERD.OK_MULTI_LINES: // Success multiple lines (+file download)
+		case netasqComm.SERVERD.WARNING_OK_MULTI_LINE: // Success multiple lines but multiple warning
+		case netasqComm.SERVERD.KO_MULTI_LINES:// Failure multiple line 
+			fileSize = '';
+			fileCRC = ''; 
+			fileName = '';
+			
+			if (netasqComm.dataFollow(data)) {
+				fileSize = data.nws.serverd.data.size;
+				fileCRC = data.nws.serverd.data.crc;
+				fileName = 'download_' + data.nws.id.replace(/\s/g, '-') + '_' + session.fw.serial;
+				
+				// We delete file if already exists
+				fs.stat(fileName, function(err, stats) {
+						if (!err) { // no erro, "something" exists
+							if (stats.isFile()) {
+								console.log('%s already exists. Deleting...', fileName);
+								fs.unlink(fileName, function(exception){
+										//console.log('in fs.unlink', exception);
+										if (exception) {
+											console.log('An error occured when trying to delete %s: ', fileName, exception);
+										} else {
+											downloadFile(session, fileName, fileWs, fileSize);
+										}
+								});
+							}
+						} else if (err.code === 'ENOENT') {
+							downloadFile(session, fileName, fileWs, fileSize);
+						} 
+				});
+				
+			} else {
+				promptCli();
+			}
+			break;
+			
+			// One line
+		case netasqComm.SERVERD.OK: // Success one line
+		case netasqComm.SERVERD.OK_SERVER_NEED_REBOOT: // Success but appliance should be restarted (in order to apply modifications) 
+		case netasqComm.SERVERD.WARNING_OK: // Success one line but one warning 
+		case netasqComm.SERVERD.KO: // Failure one line
+		case netasqComm.SERVERD.KO_LEVEL: // Administator do not have enought level to run specified command
+		case netasqComm.SERVERD.KO_LICENCE: // Appliance do not have licence option to run specified command
+			promptCli();
+			break;
+			
+			// Others
+		default:
+			promptCli();
 		}
-		console.log('Session error occured (no details)');
-		process.exit(1);
-});
-
+		break;
+		
+	case netasqComm.NWS_ERROR_CODE.INVALID_SESSION:
+	case netasqComm.NWS_ERROR_CODE.REQUEST_ERROR: 
+		console.log(netasqComm.getObjectValue('nws.msg', data));
+		break;
+		
+	default:
+		console.log(netasqComm.getObjectValue('nws.msg', data));
+		promptCli();
+	}
+}
 
 /**
 * Prompt cli
@@ -123,108 +235,10 @@ function promptCli() {
 		if (error) {
 			return;
 		}
-		session.exec(value.cmd, function(data){
-				var serverd, fileSize, fileCRC, fileName, fileWs;
-				
-				switch (netasqComm.getObjectValue('nws.code', data)) {
-				case  '100': 
-					serverd = netasqComm.getObjectValue('nws.serverd', data);
-					netasqComm.dumpServerdObject(serverd);
-					
-					switch(parseInt(serverd.ret, 10)) {
-					case netasqComm.SERVERD.OK_SERVER_WAITING_MULTI_LINES: // Success: serverd is waiting for data
-						console.log('netasqComm.SERVERD.OK_SERVER_WAITING_MULTI_LINES');
-						prompt.get([
-								{
-									message: 'File to upload',    
-									name: 'file', 
-									default: '/Users/sebastiend/Dev/perso/node/node-netasq-comm/download_config-backup-list=object_V50XXA0H0000003'           
-								}
-						], function (err, result) {
-							if (err) {
-								return;
-							}
-							console.log('Uploading to %s...', result.file);
-							session.upload(result.file, function() {
-									console.log('Upload done');	
-									promptCli();
-							});
-						});
-						
-						break;
-						
-						// Disconnected
-					case netasqComm.SERVERD.OK_DISCONNECTED: // Success: Session is closed
-					case netasqComm.SERVERD.KO_AUTH: // Authentication failed
-					case netasqComm.SERVERD.KO_TIMEOUT_DISCONNECTED: // Failure: timout disonnected (no activity)
-					case netasqComm.SERVERD.KO_MAXIMUM_ADMIN_REACH: // Failure: maximum administrator are connected to appliance
-						break;
-						
-						// Multiple lines
-					case netasqComm.SERVERD.OK_MULTI_LINES: // Success multiple lines (+file download)
-					case netasqComm.SERVERD.WARNING_OK_MULTI_LINE: // Success multiple lines but multiple warning
-					case netasqComm.SERVERD.KO_MULTI_LINES:// Failure multiple line 
-						fileSize = '';
-						fileCRC = ''; 
-						fileName = '';
-						
-						if (netasqComm.dataFollow(data)) {
-							fileSize = data.nws.serverd.data.size;
-							fileCRC = data.nws.serverd.data.crc;
-							fileName = 'download_' + data.nws.id.replace(/\s/g, '-') + '_' + session.fw.serial;
-							
-							// We delete file if already exists
-							fs.stat(fileName, function(err, stats) {
-									if (!err) { // no erro, "something" exists
-										if (stats.isFile()) {
-											console.log('%s already exists. Deleting...', fileName);
-											fs.unlink(fileName, function(exception){
-													//console.log('in fs.unlink', exception);
-													if (exception) {
-														console.log('An error occured when trying to delete %s: ', fileName, exception);
-													} else {
-														downloadFile(session, fileName, fileWs, fileSize);
-													}
-											});
-										}
-									} else if (err.code === 'ENOENT') {
-										downloadFile(session, fileName, fileWs, fileSize);
-									} 
-							});
-							
-						} else {
-							promptCli();
-						}
-						break;
-						
-						// One line
-					case netasqComm.SERVERD.OK: // Success one line
-					case netasqComm.SERVERD.OK_SERVER_NEED_REBOOT: // Success but appliance should be restarted (in order to apply modifications) 
-					case netasqComm.SERVERD.WARNING_OK: // Success one line but one warning 
-					case netasqComm.SERVERD.KO: // Failure one line
-					case netasqComm.SERVERD.KO_LEVEL: // Administator do not have enought level to run specified command
-					case netasqComm.SERVERD.KO_LICENCE: // Appliance do not have licence option to run specified command
-						promptCli();
-						break;
-						
-						// Others
-					default:
-						promptCli();
-					}
-					break;
-					
-				case '203': // ?
-				case '200': // no session
-					console.log(netasqComm.getObjectValue('nws.msg', data));
-					break;
-						
-				default:
-					console.log(netasqComm.getObjectValue('nws.msg', data));
-					promptCli();
-				}
-		});
+		session.exec(value.cmd, manageResponse);
 	});
 }
+
 
 function downloadFile(session, fileName, fileWs, size) {
 	// create file
@@ -236,9 +250,9 @@ function downloadFile(session, fileName, fileWs, size) {
 	fileWs.on('error', function (exception) {
 			console.log('fileWs error', exception);
 	});
-	fileWs.on('close', function () {
+	/*fileWs.on('close', function () {
 			//		console.log('fileWs close');
-	});
+	});*/
 	console.log('Download pending...');
 	session.download(fileWs, fileName, function(){
 			console.log('%s downloaded.', fileName);
@@ -259,6 +273,10 @@ function downloadFile(session, fileName, fileWs, size) {
 	});
 }
 
+
+/**
+* Prompt configuration
+*/
 prompt.start();
 
 if (session.host === '') {	
@@ -308,7 +326,4 @@ if (promptArray.length === 0) {
 			connect();
 	});
 }
-
-
-
 
