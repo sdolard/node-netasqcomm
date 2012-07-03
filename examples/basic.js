9f3db0a7227b7dffc8c50a4a193741565510b3c9
Sébasten Dolard <sdolard@gmail.com>
2012-07-03 23:07:13 +0200

    event review

--- examples/basic.js
+++ examples/basic.js
@@ -3,34 +3,50 @@ Copyright © 2011-2012 by Sebastien Dolard (sdolard@gmail.com)
 */
 
 var
+util = require('util'),
+
+//profiler = require('v8-profiler'),
+
+
 session = require('../lib/netasqcomm').createSession({
 		login: 'admin',
 		pwd: 'adminadmin',
 		host: '10.0.0.254',
+		/*pwd: 'simple1107',
+		host: '192.168.0.254',*/
 		verbose: false // true if you want debug logs
 });
 
+//console.log(profiler.takeSnapshot('foo'));
+//profiler.startProfiling('bar');                   //begin cpu profiling
 
-session.on('error', function(error) {
-		if (error) {
-			console.log('Session error (%s): %s', error.code, error.message);	
-		} else {
-			console.log('Session error occured (no details)');
+console.log(util.format('Connecting to %s...', session.host));
+session.connect(function(err) {
+		if (err) {
+			console.log(err.message);
+	//		console.log(util.inspect(profiler.stopProfiling('bar'), true, null));
+			
+	//		console.log(profiler.takeSnapshot('baz'));
+			process.exit(1);
 		}
-		process.exit(1);
-});
-
-
-session.on('connect', function(err) {
+		
 		console.log('Logged in.');
-		console.log('Session level: %s', session.sessionLevel);		
-		session.exec('help', function(response){
-				response.dumpServerdData();
-				session.exec('quit', function(response){
-						response.dumpServerdData();
+		console.log('Session level: %s', session.sessionLevel);	
+		console.log('> HELP');
+		session.exec('help', function(err, response){
+				if (err) {
+					console.log(err.message);
+				} else {
+					response.dumpServerdData();
+				}
+				console.log('> QUIT');
+				session.exec('quit', function(err, response){
+						if (err) {
+							console.log(err.message);
+						} else {
+							response.dumpServerdData();
+						}
 				});
 		});
 });
-
-session.connect();
 
